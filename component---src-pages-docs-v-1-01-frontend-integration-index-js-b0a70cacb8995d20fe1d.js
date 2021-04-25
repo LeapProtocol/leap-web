6e8cc76384a0ae8d51a86b92f59aa30f7dcbfa33
Vern <theanghv@gmail.com>
2021-04-25 11:20:03 +0800

    Updates

--- component---src-pages-docs-v-1-01-frontend-integration-index-js-b0a70cacb8995d20fe1d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{QYbr:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return a}));var o=e("q1tI"),r=e.n(o),c=e("YwZP");function a(n){var t=n.path;return r.a.createElement(c.Redirect,{from:t,to:t+"connect-to-uniswap/",noThrow:!0})}}}]);
-//# sourceMappingURL=component---src-pages-docs-v-1-01-frontend-integration-index-js-b0a70cacb8995d20fe1d.js.map
--- component---src-pages-docs-v-2-06-smart-contract-integration-index-js-85c0f07c72e8f286bf50.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[78],{hdDU:function(t,n,r){"use strict";r.r(n),r.d(n,"default",(function(){return c}));var e=r("q1tI"),o=r.n(e),a=r("YwZP");function c(t){var n=t.path;return o.a.createElement(a.Redirect,{from:n,to:n+"quick-start/",noThrow:!0})}}}]);
-//# sourceMappingURL=component---src-pages-docs-v-2-06-smart-contract-integration-index-js-85c0f07c72e8f286bf50.js.map
